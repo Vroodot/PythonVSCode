0b7d054dc858cb121d69dd4afbdd97371f204fdd
Vr00d <vroodot@gmail.com>
2025-01-26 07:12:21 -0500

    Create indoor_voice.py

---
+++ indoor_voice.py
@@ -0,0 +1,36 @@
+# indoor_voice.py
+
+# 1st attempt at the cs50p problem: [Indoor Voice](https://cs50.harvard.edu/python/2022/psets/0/indoor/)
+# Goal: Take an input String from the user and return one that isn't "yelling"
+
+
+
+# 1st, get input from the user. We also stop the end param from making a new-line
+print("Input: ", end="")
+
+# Here, we store the user's input String, make sure only the first letter is capitalized, and we'll remove all the trailing white space just for fun.
+i = input().capitalize().lstrip()
+print(i)
+
+
+"""
+Scratch Paper:
+
+Expected Behavoir
+1. Ask for input in the terminal
+2. Run the capitalize and lstrip methods on the input string
+3. Store that as `i`
+4. Print the inital input string with an "indoor voice"
+
+Notes:
+After first test, it works!! Kind of.
+If the input contains multiple sentences, only the first one will be capitalized.
+It seems that capitalize() doesn't care about sentence structure or common society.
+If I wanted to get rid of leading whitespace as well, I could just use `strip().` The l/r prefix to picks side.
+
+For the actual cs50p, they are likely wanting the use of `lower()` or `casefold()` since the goal is to only return an
+LC-String.
+
+Doc Pages Used To Answer:
+[String Methods](https://docs.python.org/3/library/stdtypes.html#string-methods)
+"""
