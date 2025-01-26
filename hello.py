55bee09420e8ebbf2a2f8797ee2b57e9228776cc
Vr00d <vroodot@gmail.com>
2025-01-25 21:33:17 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+
+def main():
+    print("Hello World!")
+    print("Following along to CS50P, I've just done 2 functions")
+
+# We define our functions above, and we call them below
+
+# Unlike gdscript, there's no need to put these calls under a _ready() or _process()
+main() 
